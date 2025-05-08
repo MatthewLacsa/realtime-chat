2572a80f1a359064bbe02bd0b71e0f8a77b0c5c5
Matthew Lacsa <mt885193@dal.ca>
2025-05-07 21:54:33 -0300

    authentication check route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
  import { generateToken } from "../lib/utils.js"
  import User from "../models/user.models.js"
  import bcrypt from "bcryptjs"
- 
+ import cloudinary from "../lib/cloudinary.js";
  export const signup = async (req, res) => {
     const{fullName, email, password} = req.body
     try {
@@ -87,5 +87,29 @@
  };
 
  export const updateProfile = async(req, res) => {
-  
+  try {
+    const {profilePic} = req.body;
+    const userId = req.user._id;
+
+    if(!profilePic) {
+      return res.status(400).json({ message: "Profile picture is required"});
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new:true})
+
+    res.status(200).json(updatedUser)
+  } catch(error) {
+    console.log("Error in updating the profile: ", error);
+    res.status(500).json({ message: "Internal server error"});
+  }
  };
+
+ export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in each checkAuth controller ", error.message);
+    res.status(500).json({ message: "Internal Server Error"});
+  }
+ }
