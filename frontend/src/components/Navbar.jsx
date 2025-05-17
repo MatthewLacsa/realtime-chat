f22de3d0736dd76746301cdf090a19ddc6406648
Matthew Lacsa <matthewlacsaa@gmail.com>
2025-05-16 22:12:52 -0300

    added logout and settings that are only shown when authenticated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useAuthStore } from "../store/useAuthStore";
-import { MessageSquare } from "lucide-react";
+import { MessageSquare, Settings, LogOut, User } from "lucide-react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
   const {logout, authUser} = useAuthStore();
@@ -15,6 +15,32 @@ const Navbar = () => {
               </div>
               <h1 className="text-lg font-bold">PlannedIt</h1>
             </Link>
+          </div>
+
+          <div className="flex items-center gap-2 ">
+          <Link
+            to={"/settings"}
+            className={`
+            btn btn-sm gap-2 transition-colors  
+              
+            `}>
+              <Settings className="w-4 h-4"/>
+              <span className="hidden sm:inline">Settings</span>
+            </Link>
+            
+            {authUser && (
+              <>
+                <Link to={"/profile"} className={`btn btn-sm gap-2`}>
+                  <User className="size-5" />
+                  <span className="hidden sm:inline">Profile</span>
+                </Link>
+
+                <button className="flex gap-2 items-center" onClick={logout}>
+                  <LogOut className="size-5" />
+                  <span className="hidden sm:inline">Logout</span>
+                </button>
+              </>
+            )}
           </div>
        </div>
       </div>
