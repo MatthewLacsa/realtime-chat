4e7176333c615179bf4390e0045309b8c9ce0209
Matthew Lacsa <matthewlacsaa@gmail.com>
2025-05-16 22:06:03 -0300

    left side of the navbar complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-
+import { useAuthStore } from "../store/useAuthStore";
+import { MessageSquare } from "lucide-react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  const {logout, authUser} = useAuthStore();
   return (
-    <div>
-      NavBar
-    </div>
+    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40
+    backdrop-blur-lg lg-base-100/80">
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex items-center justify-between h-full">
+          <div className="flex items-center gap-8">
+            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <MessageSquare className="w-5 h-5 text-primary"/>
+              </div>
+              <h1 className="text-lg font-bold">PlannedIt</h1>
+            </Link>
+          </div>
+       </div>
+      </div>
+    </header>
   )
 }
 
