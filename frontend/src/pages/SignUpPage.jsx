1339a24f3432cd32cb32556226e8d537b8f59a2e
Matthew Lacsa <matthewlacsaa@gmail.com>
2025-05-17 17:10:27 -0300

    was able to fix what was happening with toaster, forgot to import haha

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -3,6 +3,7 @@ import { useAuthStore } from '../store/useAuthStore';
 import {MessageSquare, User, Mail, Lock, Eye, EyeOff, Loader2} from "lucide-react";
 import {Link} from "react-router-dom";
 import AuthImagePattern from '../components/AuthImagePattern';
+import toast from 'react-hot-toast';
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
