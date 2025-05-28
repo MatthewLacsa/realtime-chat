16504572c1d641808a7d10fd5b5800cdd3aed8b4
Matthew Lacsa <matthewlacsaa@gmail.com>
2025-05-28 16:19:50 -0300

    added socket for subscribing to messages, used auth store to get socket current state

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,7 +1,7 @@
 import {create} from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
-
+import { useAuthStore } from "./useAuthStore";
 export const useChatStore = create((set, get) => ({
     messages: [],
     users: [],
@@ -41,6 +41,22 @@ export const useChatStore = create((set, get) => ({
             toast.error(error.response.data.message);
         }
     },
+
+    subscribeToMessages: () => {
+        const {selectedUser}= get();
+        if(!selectedUser) {
+            return;
+        }
+        const socket = useAuthStore.getState().socket;
+        socket.on("newMessage", (newMessage) => {
+            set({messages: [...get().messages, newMessage],})
+        })
+    },
+
+    unsubscribeFromMessages: () => {
+        const socket =  useAuthStore.getState().socket;
+        socket.off("newMessage");
+    },   
     /**Change things up on this later */
     setSelectedUser: (selectedUser) => set({selectedUser}),
 
