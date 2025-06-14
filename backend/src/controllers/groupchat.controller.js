4187509b233a764f9e60da27fa601e660b7b0592
Matthew Lacsa <matthewlacsaa@gmail.com>
2025-06-14 18:48:36 -0300

    few changes/planning for the group chat

---
+++ backend/src/controllers/groupchat.controller.js
@@ -0,0 +1,10 @@
+//create a group chat
+//adding users to a group chat
+//deleting users from a group chat
+//get details of a group chat
+//list all users' group chats
+//delete group chat
+//update expiry  
+/*
+seen functionality, notification, etc?
+*/
